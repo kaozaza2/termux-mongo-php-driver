c8e9722e44ad7112b620b37ba4572410df392f6b
kaozaza2 <kittisakphiri@gmail.com>
2021-01-28 10:07:29 +0700

    fix error

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
 mongofile=$PREFIX/lib/php/mongodb.so
-if [ -f $mongofile ]; then
+if [ -f "$mongofile" ]; then
     echo -e "Mongo php driver already installed!"
 else
-    if ! [ -x "$(command -v php" ]; then
+    if ! [ -x "$(command -v php)" ]; then
         echo -e "php not installed."
         echo -e "please install php by run 'pkg install php' then run this script again!"
     else
         arch=$(dpkg --print-architecture)
-        if [ $arch != "aarch64" ]; then
+        if [ "$arch" != "aarch64" ]; then
             echo -e "$arch currently not available.";
         else
-            curl -L "https://raw.githubusercontent.com/kaozaza2/termux-mongo-php-driver/main/mongodb.so" -o $mongofile
-            echo "extension=mongodb.so" >> $PREFIX/lib/php.ini
+            curl -L "https://raw.githubusercontent.com/kaozaza2/termux-mongo-php-driver/main/mongodb.so" -o "$mongofile"
+            echo "extension=mongodb.so" >> "$PREFIX/lib/php.ini"
             echo -e "Mongo php driver succeasfully installed!"
         fi
     fi
